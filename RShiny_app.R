fb62ce63ca22fce1d58e5e6bd93901e3d799a239
Valentin <valentin@Albrechts-MacBook-Pro.local>
2023-03-13 00:12:51 +0000

    Update includes new tabs and a section to add new companies

--- RShiny_app.R
+++ RShiny_app.R
@@ -1,102 +1,111 @@
 library("shiny")
 library("DT")
 
-setwd("/Users/AMINA3/OneDrive - London School of Economics/CP data lead/R-shiny portal/")
-AAAfile <- read.csv("Sample data.csv")
-colnames(AAAfile) <- c("Company Name", "Sector", "TPI ID", "Headquarter Location", 
-                       "Company Size", "CA100", "Latest year of disclosure", "Emissions - tonnes of CO2", "Activity - MWh",
-                       "Emissions Intensity")
+#setwd("/Users/AMINA3/OneDrive - London School of Economics/CP data lead/R-shiny portal/")
+#setwd('/Users/valentin/Library/CloudStorage/OneDrive-Personal/CP_Rshiny/CarbonPerformanceAssessmentData/Sample_Data.csv')
+AAAfile <- read.csv("Sample_Data.csv")
+colnames(AAAfile) <- c("Company_Name","Sector","Company_ID","HQ_Location","Company_Size","CA100+","Research_Date","Year","GHG_Value","GHG_Unit","Activity_Value","Activity_Unit","GHG_Intensity")
 row.names(AAAfile) <- AAAfile$`Company Name`
 
 ui <- fluidPage(
   ###Title
   titlePanel("Carbon Perfomance"),
   
-  sidebarLayout(
-    sidebarPanel((""),
-                 ###>>>Company select
-                 selectInput("company", "Pick a company to perform a Carbon Performance:", 
-                             choices = c("Please select company name",AAAfile$`Company Name`)),
-                 submitButton("See company details"),
-                 
-                 ##>>>Emissions Data Input
-                 
-                 selectInput("emissionsAccountingBoundary2020", "Select accounting boundary on which emissions are stated",
-                             c("Equity" = "equity",
-                               "Operational" = "operational",
-                               "Unknown"="unkown")),
-                 
-                 
-                 numericInput("yearofdisclosure","Please enter year of dislcosure",0000,2050),  
-                 
-                 textOutput("emissionsDataCollection"),
-                 numericInput("emissions2020","Emissions 2020", 0, 10000000000000000),
-                 
-                 textInput("emissionsSource2020", "Please provide the name of the source document"),
-                 numericInput("emissionsSourcePage2020","Please provide the source page",0, 10000000000000000),
-                 dateInput("emissionsSourceDate", "Source publication date:", value = "2022-06-01", format = "mm/dd/yy"),
-                 ### Source URL
-                 textAreaInput("emissionsComments2020", "Please provide any further comments including sources where appropriate"),
-                 
-                 ###>>>Company output Data Input
-                 textOutput("outputDataCollection"),
-                 numericInput("output2020","Output 2020", 0, 10000000000000000),
-                 selectInput("outputAccountingBoundary2020", "Select accounting boundary on which the output is stated",
-                             c("Equity" = "equity",
-                               "Operational" = "operational")),
-                 textInput("outputSource2020", "Please provide the name of the source document"),
-                 numericInput("outputSourcePage2020","Please provide the source page",0, 10000000000000000),
-                 textInput("outputComments2020", "Please provide any further comments including sources where appropriate"),
-                 
-                 #fileInput("additionalIntensityCalculations", "Please upload any files including additional permutations or modifications made to disclosed values. Please follow the format you can find here."),
-                 
-                 
-                 ###>>>Company intensity Data Input
-                 textOutput("intensityDataCollection"),
-                 numericInput("intensity2020","Intensity 2020", 0, 10000000000000000),
-                 selectInput("intensityAccountingBoundary2020", "Select accounting boundary on which the emissions intensity is stated",
-                             c("Equity" = "equity",
-                               "Operational" = "operational")),
-                 textInput("intensitySource2020", "Please provide the name of the source document"),
-                 numericInput("intensitySourcePage2020","Please provide the source page",0, 10000000000000000),
-                 textInput("intensityComments2020", "Please provide any further comments including sources where appropriate")),
+  tabsetPanel( 
+    tabPanel("Company Selection", 
+    selectInput("companySelection", "Pick a company to perform a Carbon Performance assessment for:", 
+                choices = c("Please select company name",AAAfile$Company_Name)),
+    selectInput("companyAssessmentDate", "Please select the assessment date for which you would like to see data. Please note, once you submit new data, a new assessment date will appear below",
+                choices = c("Please select date",AAAfile$Research_Date )),
+    submitButton("See company details"),
     
-    #output telling you difference between calculated and reported intensity
+    #plotOutput(),
+    #tableOutput(),
+    
+    textOutput("newCompanyIntro"),
+    
+    textInput("newCompanyName", "Please enter the name of the company you would like to add"),
+    textInput("newCompanyID", "Please enter the ID of the company you would like to add"),
+    selectInput("newCompanySector", "Please select the sector of the company you would like to add",
+                choices = c("Please select sector",AAAfile$Sector))
+    ),
     
     
     
-    
-    # submitButton("Submit", icon("refresh")),
-    # 
-    # 
-    # textOutput("emissionsIntensity"),
-    # 
-    # radioButtons("disclosureVSownestimate", "Is the disclosed intensity 
-    #              substantiated by own estimated intensity?", c("Yes"="yes", "No"="no"),selected ="no"),
-    # 
-    # 
-    # plotOutput("carbonPerformance")),
+    tabPanel("2019", h1("Coming soon")),
     
     
     
-    
-    
-    
-    mainPanel(h3("Company details"),
-              tableOutput("companydetails"))
-  )  
+    tabPanel("2020",
+             ###Company Details
+             
+             #tabsetpanels?
+             
+             
+             ###>>>Emissions Data Input
+             textOutput("emissionsDataCollection"),
+             numericInput("emissions2020","Emissions 2020", 0, 10000000000000000),
+             selectInput("emissionsAccountingBoundary2020", "Select accounting boundary on which emissions are stated",
+                         c("Equity" = "equity",
+                           "Operational" = "operational",
+                           "Unknown"="unkown")),
+             textInput("emissionsSource2020", "Please provide the name of the source document"),
+             numericInput("emissionsSourcePage2020","Please provide the source page",0, 10000000000000000),
+             dateInput("emissionsSourceDate", "Source publication date:", value = "2022-06-01", format = "mm/dd/yy"),
+             ### Source URL
+             textAreaInput("emissionsComments2020", "Please provide any further comments including sources where appropriate"),
+             
+             ###>>>Company output Data Input 
+             textOutput("outputDataCollection"),
+             numericInput("output2020","Output 2020", 0, 10000000000000000),
+             selectInput("outputAccountingBoundary2020", "Select accounting boundary on which the output is stated",
+                         c("Equity" = "equity",
+                           "Operational" = "operational")),
+             textInput("outputSource2020", "Please provide the name of the source document"),
+             numericInput("outputSourcePage2020","Please provide the source page",0, 10000000000000000),
+             textInput("outputComments2020", "Please provide any further comments including sources where appropriate"),
+             
+             fileInput("additionalIntensityCalculations", "Please upload any files including additional permutations or modifications made to disclosed values. Please follow the format you can find here."),
+             
+             
+             ###>>>Company intensity Data Input 
+             textOutput("intensityDataCollection"),
+             numericInput("intensity2020","Intensity 2020", 0, 10000000000000000),
+             selectInput("intensityAccountingBoundary2020", "Select accounting boundary on which the emissions intensity is stated",
+                         c("Equity" = "equity",
+                           "Operational" = "operational")),
+             textInput("intensitySource2020", "Please provide the name of the source document"),
+             numericInput("intensitySourcePage2020","Please provide the source page",0, 10000000000000000),
+             textInput("intensityComments2020", "Please provide any further comments including sources where appropriate"),
+             
+             #output telling you difference between calculated and reported intensity
+             
+             
+             
+             
+             submitButton("Submit", icon("refresh")),
+             
+             
+             textOutput("emissionsIntensity"),
+             
+             radioButtons("disclosureVSownestimate", "Is the disclosed intensity substantiated by own estimated intensity?", c("Yes"="yes", "No"="no"),selected ="no"),
+             
+             
+             plotOutput("carbonPerformance")
+    )
+  )
 )
 
 
-#   
-server <- function(input, output){
-  
-  
-  
-  output$companydetails <- renderTable({
-    AAAfile[input$company,]
+
+
+server <- function(input, output, session){
+  output$newCompanyIntro <- renderText({
+    c("If the company you are researching is not yet covered, please create an entry below:")
   })
-  
+  output$companyDetails <- renderText({
+    c("Company Name: SSE >", "Sector: Electricity Sector>", "ID: 1>", "Headquarter Location: UK>", "Company Size: Large>, CA100+ Focus Company: Yes")
+  })
   output$emissionsDataCollection <- renderText({
     c("When collecting emissions data please ensure the data only reflects scope 1 emissions from the company's owned electricity generation, exlcuding any emissions from non-electricity generation related activities (e.g. such as from a gas distribution business run by the same company) or emissions relating to electricity purchased by the company for the sole purpse of re-selling it. For more information on emission scopes please click here. Key source documents usually are: Annual reports (or 10-K's/20F's in the US), Sustainability reports or similar, Climate specific reports (e.g. climate roadmap), Transition reports, investor presentations, websites and CDP responses. In the US please consult IEEE ESG templates as a key source if provided by the company.")
   })
@@ -109,3 +118,11 @@ server <- function(input, output){
 }
 
 shinyApp(ui = ui, server = server)
+
+
+
+
+
+
+
+
